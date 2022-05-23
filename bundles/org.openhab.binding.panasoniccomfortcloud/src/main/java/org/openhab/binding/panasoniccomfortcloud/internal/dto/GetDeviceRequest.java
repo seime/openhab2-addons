fbb16ac656541d26841a7bb22237a4785fc30cfb
Arne Seime <arne.seime@gmail.com>
2022-05-23 21:51:36 +0200

    Replace slash with 'f' in urls

--- bundles/org.openhab.binding.panasoniccomfortcloud/src/main/java/org/openhab/binding/panasoniccomfortcloud/internal/dto/GetDeviceRequest.java
+++ bundles/org.openhab.binding.panasoniccomfortcloud/src/main/java/org/openhab/binding/panasoniccomfortcloud/internal/dto/GetDeviceRequest.java
@@ -32,6 +32,6 @@ public class GetDeviceRequest extends AbstractRequest {
     @Override
     public String getRequestUrl() {
         return String.format("/deviceStatus/%s",
-                URLEncoder.encode(deviceId.substring(0, deviceId.indexOf('/')), Charset.defaultCharset()));
+                URLEncoder.encode(deviceId.replaceAll("/", "f"), Charset.defaultCharset()));
     }
 }
